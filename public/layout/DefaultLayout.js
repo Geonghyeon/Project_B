1cac6c021005831a050f369be52488dbd96a5a79
yeonghun <starswb@hanmail.net>
2021-04-26 17:00:10 +0900

    source

---
+++ public/layout/DefaultLayout.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+class DefaultLayout extends Component {
+
+    render() {
+
+        return (
+            <main>
+                <div className="nav">
+                    navbar test
+                </div>
+                <div classNam="body">
+                    body test
+                </div>
+            </main>
+            
+        )
+    }
+}
+
+export default DefaultLayout;
